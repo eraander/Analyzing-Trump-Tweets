5fbf0ec2327254fc51739077607f7bcfe84a0c31
samh113 <43258786+samh113@users.noreply.github.com>
2019-04-02 17:14:35 -0400

    Add files via upload

--- tweet_to_file.py
+++ tweet_to_file.py
@@ -10,11 +10,24 @@ with open('condensed_2018.json') as json_file:
         if len(tweet_whole['text']) > 60:
             tweets[tweet_whole['id_str']] = [tweet_whole['created_at'], tweet_whole['text']]
 
-
-for tweet_id, tweet_content in tweets.items():
-    file_name = os.path.join(os.curdir, 'Tweets/trump_tweet_' + tweet_id + '.txt')
-    with open(file_name, 'w') as f:
-            f.write(tweet_content[0] + '\n')
-            sentences = nltk.sent_tokenize(tweet_content[1])
-            f.write("[Trump]\n")
-            f.write('\n'.join(sentences))
+for index, key in enumerate(tweets):
+    if index == 0:
+        file_name = os.path.join(os.curdir, 'tweets_xml/2018_trump_tweets_' + tweets[key][0][:10] + '.xml')
+        with open(file_name, 'w') as f:
+            f.write('<?xml version="1.0" encoding="UTF-8" ?>\n\n<EmotionalFactuality_v1.0>\n<TEXT><![CDATA[\n\n')
+    elif index % 20 == 0:
+        with open(file_name, 'a') as f:
+            f.write(']]></TEXT>\n<TAGS>\n</TAGS>\n</EmotionalFactuality_v1.0>')
+        file_name = os.path.join(os.curdir, 'tweets_xml/2018_trump_tweets_' + tweets[key][0][:10] + '.xml')
+        with open(file_name, 'w') as f:
+            f.write('<?xml version="1.0" encoding="UTF-8" ?>\n\n<EmotionalFactuality_v1.0>\n<TEXT><![CDATA[\n\n')
+    elif index == len(tweets)-1:
+        with open(file_name, 'a') as f:
+            f.write(']]></TEXT>\n<TAGS>\n</TAGS>\n</EmotionalFactuality_v1.0>')
+    with open(file_name, 'a') as f:
+        f.write(key + '\n')
+        f.write(tweets[key][0] + '\n')
+        f.write("[Trump]\n")
+        sentences = nltk.sent_tokenize(tweets[key][1])
+        f.write('\n'.join(sentences))
+        f.write('\n\n')
